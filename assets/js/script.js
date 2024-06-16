0342622b263ea41ae8f3afc5091f2a128502ef57
Andrey Belyakov <rayrah@live.com>
2024-06-17 00:50:21 +0100

    Center and tidy `index.html`.
    - Remove hero dots and its html structure, style classes,
    and scripts.
    - Remove unnecessary padding from `.hero-title section.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,44 +4,6 @@ var menuButton = $(".navigation-menu-btn"),
   navigationContainer = $("#navigation-container"),
   navigationOverlay = $("#navigation-overlay");
 
-/* Hero Dot Animation */
-
-function initPortfolio() {
-  //  Clone Hero Dots.
-  $.fn.duplicate = function (a, b) {
-    var c = [];
-    for (var d = 0; d < a; d++) $.merge(c, this.clone(b).get());
-    return this.pushStack(c);
-  };
-  $("<div class='hero-dot'></div>").duplicate(12).appendTo(".hero-dot-container");
-
-  function heroDotAnimation() {
-    function a(a) {
-      var b = a.length,
-        c, d;
-      while (b) {
-        d = Math.floor(Math.random() * b--);
-        c = a[b];
-        a[b] = a[d];
-        a[d] = c;
-      }
-      return a;
-    }
-    var b = $(".hero-dot");
-    $(a(b).slice(0, $(".hero-dot-container").data("ran"))).each(function (a) {
-      var bc = $(this);
-      b.removeClass("dot-visible")
-      bc.addClass("dot-visible");
-
-    });
-  }
-  setInterval(function () {
-    heroDotAnimation();
-  }, 2000);
-}
-
-initPortfolio()
-
 /* Dynamically Typing Text */
 
 ! function($) {
