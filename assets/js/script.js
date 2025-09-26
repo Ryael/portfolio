3cd91521d1a8701d72fc9e5c7408bc8e2eb2bbc3
Andrey Belyakov <rayrah@live.com>
2025-09-26 20:46:46 +0100

    Add scripts for embla carousel.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
+
 /* Global Variables */
+
+
+
 
 var menuButton = $(".navigation-menu-btn"),
   navigationContainer = $("#navigation-container"),
@@ -540,3 +544,174 @@ window.onmousemove = function (e) {
     $('.slick-current-clone-animate', featuredProjects).removeClass('slick-current-clone-animate');
   });
 
+
+const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
+  let dotNodes = []
+
+  const addDotBtnsWithClickHandlers = () => {
+    dotsNode.innerHTML = emblaApi
+      .scrollSnapList()
+      .map(() => '<button class="embla__dot" type="button"></button>')
+      .join('')
+
+    const scrollTo = (index) => {
+      emblaApi.scrollTo(index)
+    }
+
+    dotNodes = Array.from(dotsNode.querySelectorAll('.embla__dot'))
+    dotNodes.forEach((dotNode, index) => {
+      dotNode.addEventListener('click', () => scrollTo(index), false)
+    })
+  }
+
+  const toggleDotBtnsActive = () => {
+    const previous = emblaApi.previousScrollSnap()
+    const selected = emblaApi.selectedScrollSnap()
+    dotNodes[previous].classList.remove('embla__dot--selected')
+    dotNodes[selected].classList.add('embla__dot--selected')
+  }
+
+  emblaApi
+    .on('init', addDotBtnsWithClickHandlers)
+    .on('reInit', addDotBtnsWithClickHandlers)
+    .on('init', toggleDotBtnsActive)
+    .on('reInit', toggleDotBtnsActive)
+    .on('select', toggleDotBtnsActive)
+
+  return () => {
+    dotsNode.innerHTML = ''
+  }
+}
+
+const addTogglePrevNextBtnsActive = (emblaApi, prevBtn, nextBtn) => {
+  const togglePrevNextBtnsState = () => {
+    if (emblaApi.canScrollPrev()) prevBtn.removeAttribute('disabled')
+    else prevBtn.setAttribute('disabled', 'disabled')
+
+    if (emblaApi.canScrollNext()) nextBtn.removeAttribute('disabled')
+    else nextBtn.setAttribute('disabled', 'disabled')
+  }
+
+  emblaApi
+    .on('select', togglePrevNextBtnsState)
+    .on('init', togglePrevNextBtnsState)
+    .on('reInit', togglePrevNextBtnsState)
+
+  return () => {
+    prevBtn.removeAttribute('disabled')
+    nextBtn.removeAttribute('disabled')
+  }
+}
+
+const addPrevNextBtnsClickHandlers = (emblaApi, prevBtn, nextBtn) => {
+  const scrollPrev = () => {
+    emblaApi.scrollPrev()
+  }
+  const scrollNext = () => {
+    emblaApi.scrollNext()
+  }
+  prevBtn.addEventListener('click', scrollPrev, false)
+  nextBtn.addEventListener('click', scrollNext, false)
+
+  const removeTogglePrevNextBtnsActive = addTogglePrevNextBtnsActive(
+    emblaApi,
+    prevBtn,
+    nextBtn
+  )
+
+  return () => {
+    removeTogglePrevNextBtnsActive()
+    prevBtn.removeEventListener('click', scrollPrev, false)
+    nextBtn.removeEventListener('click', scrollNext, false)
+  }
+}
+
+const TWEEN_FACTOR_BASE = 0.75
+let tweenFactor = 0
+
+const numberWithinRange = (number, min, max) =>
+  Math.min(Math.max(number, min), max)
+
+const setTweenFactor = (emblaApi) => {
+  tweenFactor = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length
+}
+
+const tweenOpacity = (emblaApi, eventName) => {
+  const engine = emblaApi.internalEngine()
+  const scrollProgress = emblaApi.scrollProgress()
+  const slidesInView = emblaApi.slidesInView()
+  const isScrollEvent = eventName === 'scroll'
+
+  emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+    let diffToTarget = scrollSnap - scrollProgress
+    const slidesInSnap = engine.slideRegistry[snapIndex]
+
+    slidesInSnap.forEach((slideIndex) => {
+      if (isScrollEvent && !slidesInView.includes(slideIndex)) return
+
+      if (engine.options.loop) {
+        engine.slideLooper.loopPoints.forEach((loopItem) => {
+          const target = loopItem.target()
+
+          if (slideIndex === loopItem.index && target !== 0) {
+            const sign = Math.sign(target)
+
+            if (sign === -1) {
+              diffToTarget = scrollSnap - (1 + scrollProgress)
+            }
+            if (sign === 1) {
+              diffToTarget = scrollSnap + (1 - scrollProgress)
+            }
+          }
+        })
+      }
+
+      const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor)
+      const opacity = numberWithinRange(tweenValue, 0, 1).toString()
+      emblaApi.slideNodes()[slideIndex].style.opacity = opacity
+    })
+  })
+}
+
+const setupTweenOpacity = (emblaApi) => {
+  const slideNodes = emblaApi.slideNodes()
+
+  setTweenFactor(emblaApi)
+  tweenOpacity(emblaApi)
+
+  emblaApi
+    .on('reInit', setTweenFactor)
+    .on('reInit', tweenOpacity)
+    .on('scroll', tweenOpacity)
+    .on('slideFocus', tweenOpacity)
+
+  return () => {
+    slideNodes.forEach((slide) => slide.removeAttribute('style'))
+  }
+}
+
+const OPTIONS = { loop: true }
+
+const emblaNode = document.querySelector('.embla')
+const viewportNode = emblaNode.querySelector('.embla__viewport')
+const prevBtn = emblaNode.querySelector('.embla__button--prev')
+const nextBtn = emblaNode.querySelector('.embla__button--next')
+const dotsNode = document.querySelector('.embla__dots')
+
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS)
+const removeTweenOpacity = setupTweenOpacity(emblaApi)
+
+const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+  emblaApi,
+  prevBtn,
+  nextBtn
+)
+const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+  emblaApi,
+  dotsNode
+)
+
+emblaApi
+  .on('destroy', removeTweenOpacity)
+  .on('destroy', removePrevNextBtnsClickHandlers)
+  .on('destroy', removeDotBtnsAndClickHandlers)
